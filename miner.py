f625a6a757d0dc036d1a72c8b39372dc431eea5a
paliou05 <paliou05@gmail.com>
2018-12-03 11:20:52 +0200

    Update miner.py

--- miner.py
+++ miner.py
@@ -7,8 +7,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from db_insert import Database_Inserting	
 
-auth = tweepy.OAuthHandler("KKwsfmkUMpv8ag7ptJPui5Xp8", "w6Fx0fPl7rfNayZXPgQ3crIAsWaNaENmtQJZJSnGgLJtoWs1Wt")
-auth.set_access_token("4178185372-Eq4HWoHZtOu1e8uizQhvEKF8ylRAqmBAf7zN2LK", "cC7RTyjoeQr68zAd1Dq6lhtnwAUO6AUQO2GUZow8EdKBC")
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
 api = tweepy.API(auth)
 
 
